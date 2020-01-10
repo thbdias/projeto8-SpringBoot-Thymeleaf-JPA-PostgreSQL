40a69a461db7f12f74f78ced2ab2a96a1f553523
thbdias <thiagobalbino91@gmail.com>
2020-01-10 12:52:53 -0300

    configurando JPA + Banco de Dados

--- src/main/java/curso/springboot/springboot/SpringbootApplication.java
+++ src/main/java/curso/springboot/springboot/SpringbootApplication.java
@@ -2,8 +2,10 @@ package curso.springboot.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "curso.springboot.model")
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
